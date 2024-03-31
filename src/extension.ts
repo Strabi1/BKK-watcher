2a007ff15da4c2822cecb0e847bb17c4162565c0
Stráhl Balázs <balazs.strahl@gmail.com>
2024-03-31 11:05:47 +0200

    Split code

--- src/extension.ts
+++ src/extension.ts
@@ -58,85 +58,116 @@ type TripProperties = {
 	startTime?: string,
 }
 
+const url = `https://go.bkk.hu/api/query/v1/ws/gtfs-rt/full/TripUpdates.pb?key=${apiKey}`;
+
+const routeId = '1340';             // Bus 134
+const stopId = 'F00496';            // Huba street
+
+let folder: string;
+let protoFile: string;
 
 export function activate(context: vscode.ExtensionContext) {
+	folder = vscode.workspace.workspaceFolders![0].uri.fsPath;
+	protoFile = path.join(folder, "gtfs-realtime.proto");
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand('bkkwatcher.watch', async () => {
-			
-			const url = `https://go.bkk.hu/api/query/v1/ws/gtfs-rt/full/TripUpdates.pb?key=${apiKey}`;
-
-			const routeId = '1340';             // Bus 134
-			const stopId = 'F00496';            // Huba street
-			
-			const folder = vscode.workspace.workspaceFolders![0].uri.fsPath;
-			// const fileName = path.join(folder, "TripUpdates.pb");
-			const protoFile = path.join(folder, "gtfs-realtime.proto");
-
-			try {
-				const response = await axios.get(url, { responseType: 'arraybuffer' });
-				const buffer = response.data;
-
-				const root = await protobuf.load(protoFile);
-				const FeedMessage = root.lookupType("transit_realtime.FeedMessage");
-
-				const message = FeedMessage.decode(buffer) as any as FeedMessage;
-
-				let times: number[] = [];
-
-				if (message.entity) {
-					message.entity.forEach((entity: FeedEntity) => {
-						if(entity?.tripUpdate?.trip?.routeId == routeId) {
-							entity?.tripUpdate?.stopTimeUpdate.forEach((stopTime: StopTimeUpdate) => {
-								if(stopTime.stopId == stopId) {
-									if(stopTime.departure?.time != undefined) {
-										const time = stopTime.departure.time;
-										times.push(time);
-									}
-								}
-							});
-						}
-					});
-				}
-
-				if(times.length) {
-					times.sort();
-
-					times.forEach(time  => {
-						const date = new Date(time*1000);
-						const now = new Date();
-
-						const diff = date.getTime() - now.getTime();
-						const diffDate = new Date(diff);
-						diffDate.getTimezoneOffset()
-						let diffStr: string = '';
-
-						if(diff < -30 * 1000)
-							return;
-
-						if(diff < 0)
-							diffStr = 'now';
-						else {
-							if(diffDate.getHours() + diffDate.getTimezoneOffset()/60)
-								diffStr = (diffDate.getHours()+diffDate.getTimezoneOffset()/60).toString().padStart(2, '0')+ ':';
-
-							diffStr += diffDate.getMinutes().toString().padStart(2, '0')
-								+ ':' + diffDate.getSeconds().toString().padStart(2, '0');
-						}
-
-						const timeStr = date.getHours().toString().padStart(2, '0')
-							+ ':' + date.getMinutes().toString().padStart(2, '0')
-							+ ':' + date.getSeconds().toString().padStart(2, '0')
-							+ ' (' + diffStr + ')';
-				
-						console.log(timeStr);
-					})
-				}
-
-			} catch(err: any) {
-				console.log(`Error fetching trip updates: ${err}`);
-			}
+			await readAndProcessTrips();
 		})
 	);
 }
 
 export function deactivate() {}
+
+async function readAndProcessTrips() {
+	const message = await readTrips();
+
+	if(message == undefined)
+		return;
+
+	const times = sortTrips(message);
+	const timesStr = createTimeString(times);
+	
+	console.log(timesStr);
+}
+
+async function readTrips(): Promise<FeedMessage | undefined> {
+	try {
+		const response = await axios.get(url, { responseType: 'arraybuffer' });
+		const buffer = response.data;
+
+		const root = await protobuf.load(protoFile);
+		const FeedMessage = root.lookupType("transit_realtime.FeedMessage");
+
+		const message = FeedMessage.decode(buffer) as any as FeedMessage;
+
+		return message;
+	} catch(err: any) {
+		console.log(`bkk api read error. Errorcode: ${err}`);
+		return undefined;
+	}
+}
+
+function sortTrips(message: FeedMessage): number[] {
+	let times: number[] = [];
+
+	if (message.entity) {
+		message.entity.forEach((entity: FeedEntity) => {
+			if(entity?.tripUpdate?.trip?.routeId == routeId) {
+				entity?.tripUpdate?.stopTimeUpdate.forEach((stopTime: StopTimeUpdate) => {
+					if(stopTime.stopId == stopId) {
+						if(stopTime.departure?.time != undefined) {
+							const time = stopTime.departure.time;
+							times.push(time);
+						}
+					}
+				});
+			}
+		});
+
+		if(times.length)
+			times.sort();
+	}
+
+	return times;
+}
+
+function createTimeString(times: number[]): string {
+	if(!times.length)
+		return String();
+
+	let timesStr = "";
+
+	times.forEach(time  => {
+		const date = new Date(time*1000);
+		const now = new Date();
+
+		const diff = date.getTime() - now.getTime();
+		const diffDate = new Date(diff);
+		diffDate.getTimezoneOffset()
+		let diffStr: string = '';
+
+		if(diff < -30 * 1000)
+			return;
+
+		if(diff < 0)
+			diffStr = 'now';
+		else {
+			if(diffDate.getHours() + diffDate.getTimezoneOffset()/60)
+				diffStr = (diffDate.getHours()+diffDate.getTimezoneOffset()/60).toString().padStart(2, '0')+ ':';
+
+			diffStr += diffDate.getMinutes().toString().padStart(2, '0')
+				+ ':' + diffDate.getSeconds().toString().padStart(2, '0');
+		}
+
+		const timeStr = date.getHours().toString().padStart(2, '0')
+			+ ':' + date.getMinutes().toString().padStart(2, '0')
+			+ ':' + date.getSeconds().toString().padStart(2, '0')
+			+ ' (' + diffStr + ')';
+
+		// console.log(timeStr);
+		timesStr += timeStr + '\n';
+	})
+
+	return timesStr;
+}
